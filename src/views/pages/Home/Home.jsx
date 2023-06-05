050c554db9defa1165fa97f9c177e3fca4d07763
AErlich <axel.erlich@yahoo.com.ar>
2023-06-04 23:34:21 -0300

    Refactorizo algunas partes de las reglas

--- src/views/pages/Home/Home.jsx
+++ src/views/pages/Home/Home.jsx
@@ -7,11 +7,14 @@ import constants from '../../../assets/constants';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Modal from 'react-bootstrap/Modal';
+import { crearReglasDeMotos } from '../../utils/motorDeReglas'
 
 import './Home.css';
 
 const { Engine } = require('json-rules-engine')
 const engine = new Engine()
+
+crearReglasDeMotos(engine);
 
 const Home = () => {
 
@@ -62,61 +65,8 @@ const Home = () => {
         let errors = {};
 
         if (validate(values)) {
-            //Motor de reglas
-
-            if (!inicalizado) { //Tal vez es mejor poner esto en el "starter" pero ahora no me puse a buscar si eso existe
-                //              USO       Experiencia/Presupuesto       Moto/mensaje
-                //R1
-                CrearReglaSimple('Recreativo', 'Alta', 'Alto', 'MT-10, CB1000R, Diavel 1200, H2', 'Al tener alto presupuesto y alta experiencia para uso recreativo cualquier modelo esta permitido sin limite de HP. Basicamente va a depender de los gustos personales de cada usuario.');
-                //R2
-                CrearReglaSimple('Recreativo', 'Alta', 'Medio', '700 CLX, 650NK, 752S, TNT600, Interceptor 650, Continental 650, MT07', 'alta experiencia, se busca lo mas divertido con un presupuesto medio.');
-                //R3
-                CrearReglaSimple('Recreativo', 'Alta', 'Bajo', '400NK, 302S, 500R, Dominar 400', 'alta experiencia, se busca lo mas divertido posible con un presupuesto bajo.');
-                //R4
-                CrearReglaSimple('Recreativo', 'Medio', 'Alto', 'MT-09, XSR900, GSX750, Z900, Z900RS, 701 Supermoto, DUKE 790, Hypermotard 950', 'Alto presupuesto, experiencia media, se busca algo que tenga como mucho 120 HP sin importar su precio.');
-                //R5
-                CrearReglaSimple('Recreativo', 'Medio', 'Medio', '700 CLX, 650NK, 752S, TNT600, Vitpilen 701, Svartpilen 701, Z650, MT-07, XSR700', 'Experiencia media, se busca algo que tenga como mucho 120 HP con un presupuesto bajo.');
-                //R6
-                CrearReglaSimple('Recreativo', 'Medio', 'Bajo', '400NK, 302S, 500R, CBR300, CB300R, Dominar 400', 'Experiencia media, se busca algo que tenga como mucho 120 HP con un presupuesto bajo.');
-                //R7
-                CrearReglaSimple('Recreativo', 'Baja', 'Alto', 'DUKE 390, MT-03, Ninja 400, Z400, CB500F, YZF-R3, Svartpilen 401, Vitpilen 401', 'Al tener baja experiencia, se buscan motos que tengan a lo sumo 50 HP, sin importar su precio, por lo que se apunta a las motos mas premium de este segmento.');
-                //R8
-                CrearReglaSimple('Recreativo', 'Baja', 'Medio', '400NK, 302S, 500R, CBR300, CB300R, Duke 250, Dominar 400', 'Al tener baja experiencia, se buscan motos que tengan a lo sumo 50 HP, con un presupuesto medio.');
-                //R9
-                CrearReglaSimple('Recreativo', 'Baja', 'Bajo', 'Rouser 200, DUKE 200, CB250, Svartpilen 200, 300NK, 300R, LEONCINO 250, FZ25, Dominar 250', 'Al tener baja experiencia, se buscan motos que tengan a lo sumo 50 HP, con un presupuesto bajo.');
-                //R10
-                CrearReglaSimpleSinExperiencia('Competicion', 'Alto', 'YZF-R1, CBR1000RR, ZX10R, Panigale V4, S1000RR, YZ450F, CRF450R, KX450, 350 SX, FE 450, RMZ450, TE 300 (2T)', 'Para uso competitivo existen dos principales ramas, las motos de velocidad o las motos de tierra, ambas se encuentra en espectros separados, una apunta a mayor potencia con 4 cilidros y la otra a mayor torque con un monocilindrico.');
-                //R11
-                CrearReglaSimpleSinExperiencia('Competicion', 'Medio', 'YZF-R6, ZX6R, 350 EXC, FE 250, Panigale V2, YZ250F, TE 250, CRF250R', 'Para uso competitivo existen dos principales ramas, las motos de velocidad o las motos de tierra, ambas se encuentra en espectros separados, una apunta a mayor potencia con 4 cilidros y la otra a mayor torque con un monocilindrico. En este caso al ser de presupuesto medio, se asumo que se no se apunta a la maxima categoria de cada tipo de competicion, sino a una segunda categoria.');
-                //R12
-                CrearReglaSimpleSinExperiencia('Competicion', 'Bajo', 'YZF-R3, NINJA 400, RC390, RC200, XTZ 250, XR250, KX250, YZ125', 'Para uso competitivo existen dos principales ramas, las motos de velocidad o las motos de tierra, ambas se encuentra en espectros separados, una apunta a mayor potencia con 4 cilidros y la otra a mayor torque con un monocilindrico. En este caso al ser de presupuesto medio, se asume que se apunta a una categoria de entrada a cada tipo de competicion.');
-                //R13
-                CrearReglaSimpleSinExperiencia('Laboral', 'Alto', 'moFZ 160, XR190, CB 190, XTZ 125, 180S, NMAX, PCX150to12', 'Las motos para uso laboral estan asociadas inherentemente al bajo costo de mantenimiento, pero al tener presupuesto alto, se busca tener la mayor comodidad posible en la moto.');
-                //R14
-                CrearReglaSimpleSinExperiencia('Laboral', 'Medio', 'YBR 125, XR150, TNT15, GLH150', 'Las motos para uso laboral estan asociadas inherentemente al bajo costo de mantenimiento, pero al tener presupuesto medio, se busca tener la mayor comodidad posible en la moto con un presupuesto medio.');
-                //R15
-                CrearReglaSimpleSinExperiencia('Laboral', 'Bajo', 'AX100, GN125, Crypton, Wave, Boxer, V15', 'Se buscan las motos mas economicas, tanto para comprar como para mantener.');
-                //R16
-                CrearReglaSimple('Viajes', 'Alta', 'Alto', 'moto15', '');
-                //R17
-                CrearReglaSimple('Viajes', 'Alta', 'Medio', 'moto16', '');
-                //R18
-                CrearReglaSimple('Viajes', 'Alta', 'Bajo', 'moto17', '');
-                //R19
-                CrearReglaSimple('Viajes', 'Medio', 'Bajo', 'moto18', '');
-                //R20
-                CrearReglaSimple('Viajes', 'Medio', 'Bajo', 'moto19', '');
-                //R21
-                CrearReglaSimple('Viajes', 'Medio', 'Bajo', 'moto20', '');
-
-                CrearReglaTroncal('Recreativo', true, true)
-                CrearReglaTroncal('Competicion', false, true)
-                CrearReglaTroncal('Laboral', false, true)
-
-                inicalizado = true;
-            }
-
-            var facts = { experiencia: values.experiencia,
+            var facts = { 
+                experiencia: values.experiencia,
                 presupuesto: values.presupuesto,
                 altura: values.altura,
                 uso: values.uso,
@@ -136,97 +86,15 @@ const Home = () => {
             const { events } = await engine.run(facts)
 
             if( events.length > 0) {
+                debugger
                 const firstElement = events[0]
                 setShowExperience(firstElement.params.showExperience)
                 setShowBudget(firstElement.params.showBudget)
-                debugger
                 if(firstElement.params.opcion){
                     setVehicle(firstElement)
                     setShow(true);
                 }
             }
-
-            debugger
-        }
-
-        function CrearReglaTroncal(uso, showExperience, showBudget) {
-            engine.addRule({
-                conditions: {
-                    all: [
-                        {
-                            fact: 'uso',
-                            operator: 'equal',
-                            value: uso
-                        },
-                    ]
-                },
-                event: {
-                    type: 'message',
-                    params: {
-                        opcion: null,
-                        motivo: null,
-                        showExperience: showExperience,
-                        showBudget: showBudget,
-                    }
-                }
-            });
-        }
-
-        function CrearReglaSimple(uso, experiencia, presupuesto, moto, mensaje) {
-            engine.addRule({
-                conditions: {
-                    all: [{
-                        fact: 'uso',
-                        operator: 'equal',
-                        value: uso
-                    },
-                    {
-                        fact: 'experiencia',
-                        operator: 'equal',
-                        value: experiencia
-                    },
-                    {
-                        fact: 'presupuesto',
-                        operator: 'equal',
-                        value: presupuesto
-                    }]
-                },
-                event: {
-                    type: 'message',
-                    params: {
-                        opcion: moto,
-                        motivo: mensaje,
-                        showExperience:true,
-                        showBudget:true,
-                    }
-                }
-            });
-        }
-
-        function CrearReglaSimpleSinExperiencia(uso, presupuesto, moto, mensaje) {
-            engine.addRule({
-                conditions: {
-                    all: [{
-                        fact: 'uso',
-                        operator: 'equal',
-                        value: uso
-                    },
-                    {
-                        fact: 'presupuesto',
-                        operator: 'equal',
-                        value: presupuesto
-                    }]
-                },
-                event: {
-                    type: 'message',
-                    params: {
-                        opcion: moto,
-                        motivo: mensaje,
-                        showExperience:false,
-                        showBudget:true,
-                    }
-                }
-            });
         }
     }
 
@@ -251,7 +119,7 @@ const Home = () => {
                         Motos
                     </div>
                     <div className='section-intro-sub-title'>
-                        EL vehículo para VOS
+                        El vehículo para vos
                     </div>
                 </Parallax>
             </div>
@@ -274,6 +142,8 @@ const Home = () => {
                                 </Col>
                             </Form.Group>
                         </Col>
+                    </Row>
+                    <Row>
                         {showBudget?<Col>
                             <Form.Group as={Row} className="mb-3" controlId="formPresupuesto">
                                 <Form.Label column>Presupuesto</Form.Label>
@@ -292,6 +162,8 @@ const Home = () => {
                                 </Col>
                             </Form.Group>
                         </Col>:null}
+                    </Row>
+                    <Row>
                         {showExperience?<Col>
                             <Form.Group as={Row} className="mb-3" controlId="formExperiancia">
                                 <Form.Label column >Cuanta experiancia tenes</Form.Label>
@@ -495,7 +367,8 @@ const Home = () => {
                     <Modal.Title>Encontramos su Moto</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    Su moto es {vehicle?.params.opcion}. Por el siguiente motivo: {vehicle?.params.motivo}
+                    Los modelos recomendados para usted son: {vehicle?.params.opcion}. 
+                    Por el siguiente motivo: {vehicle?.params.motivo}
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleClose}>
@@ -506,5 +379,6 @@ const Home = () => {
         </>
     )
 }
+
 
 export default Home;
